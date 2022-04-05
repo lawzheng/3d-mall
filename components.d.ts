7dc9032c19b158b15759ec25d6cb4fda759af9f8
lawzheng <602285510@qq.com>
2022-04-05 18:02:23 +0800

    add header

--- components.d.ts
+++ components.d.ts
@@ -5,6 +5,12 @@
 declare module 'vue' {
   export interface GlobalComponents {
     AButton: typeof import('ant-design-vue/es')['Button']
+    AInputSearch: typeof import('ant-design-vue/es')['InputSearch']
+    AMenu: typeof import('ant-design-vue/es')['Menu']
+    AMenuItem: typeof import('ant-design-vue/es')['MenuItem']
+    AMenuItemGroup: typeof import('ant-design-vue/es')['MenuItemGroup']
+    ASubMenu: typeof import('ant-design-vue/es')['SubMenu']
+    Header: typeof import('./src/components/Header.vue')['default']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     HomeCom: typeof import('./src/components/HomeCom.vue')['default']
     Loading: typeof import('./src/components/Loading.vue')['default']
--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
+  <Header/>
   <router-view></router-view>
 </template>
 
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import Header from './components/Header.vue'
 </script>
 
 <style>
